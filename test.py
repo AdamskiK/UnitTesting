038c897952fcf206aaa1eccde77a830fd8bd9725
dams822 <dams822@gmail.com>
2018-11-23 22:52:31 +0100

    changing some values - checking proper contribution scoring

--- test.py
+++ test.py
@@ -17,9 +17,9 @@ class TestSum(unittest.TestCase):
         """
         Test that it can sum a list of fractions
         """
-        data = [Fraction(1, 4), Fraction(1, 4), Fraction(3, 6)]
+        data = [Fraction(2, 4), Fraction(2, 4), Fraction(3, 6)]
         result = sum(data)
-        self.assertEqual(result, 1)
+        self.assertEqual(result, 1.5)
 
     def test_bad_type(self):
         data = "banana"
